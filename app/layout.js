0f100ce22e738bdd5dddf7123620c59d9a0f99ad
Gustavo Polli <gugalupolli@gmail.com>
2024-09-02 20:05:00 -0300

    created project

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import { Header } from "@/components/header/header";
+import { Footer } from "@/components/footer/footer";
+
+export const metadata = {
+  title: "Leilão Online",
+  description: "Leilão Online",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
